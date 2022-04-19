b5eaebbaba3a10c8d896363c22481cd63af0546d
Endercheif <reet22singh@gmail.com>
2022-04-18 22:48:24 -0700

    Styling + Export Button

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -12,67 +12,86 @@ export default function Controls({
 
   return (
     <div className="controls">
+      <div className="subcontrols">
+        <button
+          onClick={() =>
+            localStorage.setItem("table", JSON.stringify(tableHook.data))
+          }
+        >
+          Save
+        </button>
+        <button
+          onClick={() =>
+            tableHook.setData(JSON.parse(localStorage.getItem("table") || "[]"))
+          }
+        >
+          Load
+        </button>
+      </div>
+      <div className="subcontrols">
+        <button
+          onClick={() => {
+            const newData = tableHook.data.slice();
+            newData.push({ id: crypto.randomUUID(), table: [] });
+            tableHook.setData(newData);
+          }}
+        >
+          New Table
+        </button>
+      </div>
+      <div className="subcontrols">
+        <button
+          onClick={() => {
+            tableHook.addPerson(name);
+            temp = JSON.stringify(tableHook.data);
+            tableHook.setData([]);
+            tableHook.setData(JSON.parse(temp));
+            setName("");
+          }}
+        >
+          New Person
+        </button>
+        <input
+          type="text"
+          name="name"
+          id="new-person"
+          placeholder="person name"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+      </div>
+      <div className="subcontrols">
+        <button
+          onClick={() => {
+            tableHook.setData(JSON.parse(data as any));
+          }}
+        >
+          Import Data
+        </button>
+        <input
+          type="text"
+          name="imp-data"
+          id="imp-data"
+          placeholder="import data"
+          value={data}
+          onChange={(e) => {
+            setData(e.target.value);
+          }}
+        />
+      </div>
       <button
-        onClick={() =>
-          localStorage.setItem("table", JSON.stringify(tableHook.data))
-        }
-      >
-        Save
-      </button>
-      <button
-        onClick={() =>
-          tableHook.setData(JSON.parse(localStorage.getItem("table") || "[]"))
-        }
-      >
-        Load
-      </button>
-      <button
-        onClick={() => {
-          const newData = tableHook.data.slice();
-          newData.push({ id: crypto.randomUUID(), table: [] });
-          tableHook.setData(newData);
+        onClick={async () => {
+          await navigator.clipboard.writeText(
+            `https://${window.location.host}?${escape(
+              JSON.stringify(tableHook.data)
+            )}`
+          );
         }}
       >
-        New Table
+        Export (click to copy to clipboard)
       </button>
-      <button
-        onClick={() => {
-          tableHook.addPerson(name);
-          temp = JSON.stringify(tableHook.data);
-          tableHook.setData([]);
-          tableHook.setData(JSON.parse(temp));
-          setName("");
-        }}
-      >
-        New Person
-      </button>
-      <input
-        type="text"
-        name="name"
-        id="new-person"
-        placeholder="person name"
-        value={name}
-        onChange={(e) => {
-          setName(e.target.value);
-        }}
-      />
-      <button
-        onClick={() => {
-          tableHook.setData(JSON.parse(data as any));
-        }}
-      >
-        Import Data
-      </button>
-      <input
-        type="text"
-        name="imp-data"
-        id="imp-data"
-        placeholder="import data"
-        value={data}
-        onChange={(e) => {
-          setData(e.target.value);
-        }}
-      />
     </div>
   );
 }
